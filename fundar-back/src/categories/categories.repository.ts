374e6ad95b9906684744044295ec177c344eec66
Maximo <maximosposetti@hotmail.com>
2025-10-22 19:51:28 -0300

    0.2.3

---
+++ fundar-back/src/categories/categories.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class CategoriesRepository {
+    private categories = [
+    {
+      id: 1,
+      categoryName: 'Category 1',
+    },
+    {
+      id: 2,
+      categoryName: 'Category 2',
+    },
+    {
+      id: 3,
+      categoryName: 'Category 3',
+    },
+  ];
+}
