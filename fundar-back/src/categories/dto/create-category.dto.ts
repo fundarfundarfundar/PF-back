b6bb066eb4212d51eb1bb124ee3f885e287569b9
Maximo <maximosposetti@hotmail.com>
2025-10-31 16:29:59 -0300

    4.0.2

--- fundar-back/src/categories/dto/create-category.dto.ts
+++ fundar-back/src/categories/dto/create-category.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Category name is required' })
+  @MaxLength(50, { message: 'Name cannot exceed 50 characters.' })
+  name: string;
+}
