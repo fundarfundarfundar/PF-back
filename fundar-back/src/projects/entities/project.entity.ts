2ee781740a3609bb1f20d7b851d9123352d02b8c
JCesarAguilar <jcesar.a.15@gmail.com>
2025-11-02 09:07:53 -0500

    modificacion en project/entities para revision, ok

--- fundar-back/src/projects/entities/project.entity.ts
+++ fundar-back/src/projects/entities/project.entity.ts
@@ -1,3 +1,49 @@
+// import { Category } from 'src/categories/entities/category.entity';
+// import { Donation } from 'src/donations/entities/donation.entity';
+// import {
+//   Column,
+//   Entity,
+//   JoinColumn,
+//   ManyToOne,
+//   OneToMany,
+//   PrimaryGeneratedColumn,
+// } from 'typeorm';
+
+// @Entity()
+// export class Project {
+//   @PrimaryGeneratedColumn('uuid')
+//   id: string;
+
+//   @Column()
+//   title: string;
+
+//   @Column()
+//   description: string;
+
+//   @Column()
+//   country: string;
+
+//   @Column({ nullable: true } )
+//   resume: string;
+
+//   @Column({ type: 'float', default: 0 })
+//   goalAmount: number;
+
+//   @Column({ type: 'float', default: 0 })
+//   currentAmount: number;
+
+//   @Column('text', { array: true, default: [] })
+//   imageUrls: string[];
+
+//   @OneToMany(() => Donation, (donation) => donation.project)
+//   donations: Donation[];
+
+//   @ManyToOne(() => Category, (category) => category.projects, {
+//     onDelete: 'CASCADE',
+//   })
+//   @JoinColumn({ name: 'category_id' })
+//   category: Category;
+// }
 import { Category } from 'src/categories/entities/category.entity';
 import { Donation } from 'src/donations/entities/donation.entity';
 import {
@@ -17,14 +63,14 @@ export class Project {
   @Column()
   title: string;
 
-  @Column()
+  @Column({ length: 180 })
+  resume: string;
+
+  @Column({ length: 600 })
   description: string;
 
   @Column()
   country: string;
-
-  @Column({ nullable: true } )
-  resume: string;
 
   @Column({ type: 'float', default: 0 })
   goalAmount: number;
@@ -34,6 +80,12 @@ export class Project {
 
   @Column('text', { array: true, default: [] })
   imageUrls: string[];
+
+  @Column({ default: 'active' })
+  status: 'active' | 'inactive';
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 
   @OneToMany(() => Donation, (donation) => donation.project)
   donations: Donation[];
