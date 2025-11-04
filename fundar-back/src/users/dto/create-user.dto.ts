d0ed0c2efcaaf59c8c26c6ead2ac6e1fd1cf8d00
Francisco Sequeira <franciscosequeira879@gmail.com>
2025-11-04 14:56:14 -0300

    modificacion de campos para register

--- fundar-back/src/users/dto/create-user.dto.ts
+++ fundar-back/src/users/dto/create-user.dto.ts
@@ -11,59 +11,71 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  // @IsNotEmpty()
+  // @IsString()
+  // @MinLength(3)
+  // @MaxLength(80)
+  // name: string;
+
+  // @IsNotEmpty()
+  // @IsEmail()
+  // email: string;
+
+  // @IsString()
+  // @MinLength(8)
+  // @MaxLength(15)
+  // @IsStrongPassword({
+  //   minUppercase: 1,
+  //   minLowercase: 1,
+  //   minNumbers: 1,
+  // })
+  // @Matches(/^[A-Za-z\d!@#$%^&*]+$/, {
+  //   message:
+  //     'La contraseña solo puede contener los siguientes símbolos: !@#$%^&*',
+  // })
+  // @Matches(/[!@#$%^&*]/, {
+  //   message: 'La contraseña debe contener al menos un símbolo: !@#$%^&*',
+  // })
+  // password: string;
   @IsNotEmpty()
   @IsString()
-  @MinLength(3)
-  @MaxLength(80)
   name: string;
 
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
-  @MinLength(8)
-  @MaxLength(15)
-  @IsStrongPassword({
-    minUppercase: 1,
-    minLowercase: 1,
-    minNumbers: 1,
-  })
-  @Matches(/^[A-Za-z\d!@#$%^&*]+$/, {
-    message:
-      'La contraseña solo puede contener los siguientes símbolos: !@#$%^&*',
-  })
-  @Matches(/[!@#$%^&*]/, {
-    message: 'La contraseña debe contener al menos un símbolo: !@#$%^&*',
-  })
+  @MinLength(6)
   password: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(3)
-  @MaxLength(80)
-  address: string;
+  // @IsNotEmpty()
+  // @IsString()
+  // @MinLength(3)
+  // @MaxLength(80)
+  address?: string;
 
-  @IsNotEmpty()
-  @IsNumber()
-  @IsString()
-  phone: string;
+  // @IsNotEmpty()
+  // @IsNumber()
+  // @IsString()
+  phone?: string;
 
-  @IsString()
-  @MinLength(5)
-  @MaxLength(20)
-  country: string;
+  // @IsString()
+  // @MinLength(5)
+  // @MaxLength(20)
+  country?: string;
 
-  @IsString()
-  @MinLength(5)
-  @MaxLength(20)
-  city: string;
+  // @IsString()
+  // @MinLength(5)
+  // @MaxLength(20)
+  city?: string;
 
-  @IsNotEmpty()
-  @IsDateString()
-  birthDate: Date;
+  // @IsNotEmpty()
+  // @IsDateString()
+  birthDate?: Date;
 
-  @IsNotEmpty()
-  @IsString()
-  role: 'admin' | 'user';
+  // @IsNotEmpty()
+  // @IsString()
+  role?: 'admin' | 'user';
 }
