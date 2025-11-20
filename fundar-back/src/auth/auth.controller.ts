59f918f90ade463b8f2a4dbdeeff7526e4cc12e4
Francisco Sequeira <franciscosequeira879@gmail.com>
2025-11-20 02:15:32 -0300

    modificacion auth controller

--- fundar-back/src/auth/auth.controller.ts
+++ fundar-back/src/auth/auth.controller.ts
@@ -150,7 +150,7 @@ export class AuthController {
      const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 
      return res.redirect(
-  `   ${frontendUrl}/google-success?token=${token}&email=${encodeURIComponent(email)}&firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&role=${encodeURIComponent(user.role ?? 'user')}`
+        `${frontendUrl}/google-success?token=${token}&email=${encodeURIComponent(email)}&firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&role=${encodeURIComponent(user.role ?? 'user')}`
       );
     } catch (error) {
       throw new InternalServerErrorException(error.message || 'Error in Google OAuth callback');
