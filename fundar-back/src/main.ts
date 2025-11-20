422bf49519fabae4a81b3af7b3c1b507acb1a61d
Francisco Sequeira <franciscosequeira879@gmail.com>
2025-11-20 00:25:02 -0300

    modificado de cors

--- fundar-back/src/main.ts
+++ fundar-back/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://demo2-umber-zeta.vercel.app', 
     credentials: true, 
   });
   
